b44bbe7f5e74e58d1b2166176702ab3c569815ad
maxime rondeau <maxpline83@gmail.com>
2023-10-23 19:35:19 +0200

    finish basic model + small fix

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 import re
+import numpy as np
 
 def handles_url(text):
     url_pattern = r'https?://\S+|www\.\S+'
@@ -13,13 +14,34 @@ def preprocess_text(text):
     stop_words = set(stopwords.words('english'))
 
     cleaned_text = handles_url(text)
-    # Tokenization
     tokens = word_tokenize(cleaned_text)
-    # Remove punctuation and convert to lowercase
     words = [word.lower() for word in tokens if word.isalnum()]
-    # Remove stopwords
     words = [word for word in words if word not in stop_words]
-    # Lemmatization using WordNetLemmatizer
     words = [lemmatizer.lemmatize(word) for word in words]
     
     return ' '.join(words)
+
+def get_vectors(embeddings, words, size_of_vector=300):
+    """
+    Input:
+        embeddings: the embeddings for the words
+        words: a list of words
+    Output: 
+        X: a matrix where the rows are the embeddings corresponding to the rows on the list
+        
+    """
+    X = np.zeros((1, size_of_vector))
+    for word in words:
+        eng_emb = embeddings[word]
+        X = np.row_stack((X, eng_emb))
+    X = X[1:,:]
+    return X
+
+def Series_to_list(series):
+    """
+    Input:
+        Series: a pandas Series
+    Output: 
+        a list of lists where each list is a list of words in the corresponding row of the Series        
+    """
+    return series.apply(lambda x: x.split()).to_list()
